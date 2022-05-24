08557656b7b95bc3c30bc18aa388d230af55dba5
Utkin Denis <stolen.youth@yandex.ru>
2022-05-25 03:27:34 +0700

    feat: switch categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 function Categories() {
+    const [activeIndex, setActiveIndex] = React.useState(1);
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    }
+
     return(
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>Все</li>
+                <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>Мясные</li>
+                <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>Вегетарианская</li>
+                <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>Гриль</li>
+                <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? 'active' : ''}>Острые</li>
+                <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? 'active' : ''}>Закрытые</li>
             </ul>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import logoSvg from "../assets/img/pizza-logo.svg"
 
 function Header() {
     return (
         <div className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img width="38" src="img/pizza-logo.svg" alt="Pizza logo"/>
+                    <img width="38" src={logoSvg} alt="Pizza logo"/>
                     <div>
                         <h1>React Pizza</h1>
                         <p>самая вкусная пицца во вселенной</p>
